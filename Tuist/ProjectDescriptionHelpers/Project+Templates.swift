6439fcfecdfe2c0723052be2da30f5924f67f702
Sergey Kovalenko <sergey.kovalenko@wikrgroup.com>
2021-03-27 10:25:51 +0200

    Initial commit

---
+++ Tuist/ProjectDescriptionHelpers/Project+Templates.swift
@@ -0,0 +1,85 @@
+import ProjectDescription
+
+/// Project helpers are functions that simplify the way you define your project.
+/// Share code to create targets, settings, dependencies,
+/// Create your own conventions, e.g: a func that makes sure all shared targets are "static frameworks"
+/// See https://tuist.io/docs/usage/helpers/
+
+extension Project {
+    /// Helper function to create the Project for this ExampleApp
+    public static func app(name: String, platform: Platform, additionalTargets: [String], additionalTestTargets: [String]) -> Project {
+        var targets = makeAppTargets(name: name,
+                                     platform: platform,
+                                     dependencies: additionalTargets.map { TargetDependency.target(name: $0) },
+                                     testDependencies: additionalTestTargets.map { TargetDependency.target(name: $0) })
+        targets += additionalTargets.flatMap { makeFrameworkTargets(name: $0, platform: platform) }
+        targets += additionalTestTargets.flatMap { makeTestFrameworkTargets(name: $0, platform: platform) }
+
+        return Project(name: name,
+                       organizationName: "tuist.io",
+                       targets: targets)
+    }
+
+    // MARK: - Private
+
+    /// Helper function to create a framework target and an associated unit test target
+    private static func makeFrameworkTargets(name: String, platform: Platform) -> [Target] {
+        let sources = Target(name: name,
+                             platform: platform,
+                             product: .framework,
+                             bundleId: "io.tuist.\(name)",
+                             infoPlist: .default,
+                             sources: ["Targets/\(name)/Sources/**"],
+                             resources: [],
+                             dependencies: [])
+        return [sources]
+    }
+
+    private static func makeTestFrameworkTargets(name: String, platform: Platform) -> [Target] {
+        let sources = Target(name: name,
+                             platform: platform,
+                             product: .framework,
+                             bundleId: "io.tuist.\(name)",
+                             infoPlist: .default,
+                             sources: ["Targets/\(name)/Sources/**"],
+                             resources: [],
+                             dependencies: [.cocoapods(path: ".")])
+        return [sources]
+    }
+
+    /// Helper function to create the application target and the unit test target.
+    private static func makeAppTargets(name: String, platform: Platform, dependencies: [TargetDependency], testDependencies: [TargetDependency]) -> [Target] {
+        let platform: Platform = platform
+        let infoPlist: [String: InfoPlist.Value] = [
+            "CFBundleShortVersionString": "1.0",
+            "CFBundleVersion": "1",
+            "UIMainStoryboardFile": "",
+            "UILaunchStoryboardName": "LaunchScreen",
+        ]
+
+        let mainTarget = Target(
+            name: name,
+            platform: platform,
+            product: .app,
+            bundleId: "io.tuist.\(name)",
+            infoPlist: .extendingDefault(with: infoPlist),
+            sources: ["Targets/\(name)/Sources/**"],
+            resources: ["Targets/\(name)/Resources/**"],
+            dependencies: dependencies + [.cocoapods(path: ".")]
+        )
+
+        let testTarget = Target(
+            name: "\(name)Tests",
+            platform: platform,
+            product: .unitTests,
+            bundleId: "io.tuist.\(name)Tests",
+            infoPlist: .default,
+            sources: ["Targets/\(name)/Tests/**"],
+            dependencies: [
+                .target(name: "\(name)"),
+                .cocoapods(path: ".")
+            ] + testDependencies
+        )
+        return [mainTarget, testTarget]
+    }
+}
